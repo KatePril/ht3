15c57947ccac75c5ab243a029913983f4edc469b
KatePril <ket14pril14@gmail.com>
2023-05-06 14:21:33 +0300

    On branch master Your branch is up to date with 'origin/master'.

--- module/functions.py
+++ module/functions.py
@@ -1,18 +1,23 @@
 from module.schedule import schedule
+from module.books import books
 
 def collect_the_bag():
     day_schedule = get_day_schedule()
+    print(print_books(books))
     collected_books = []
-    while len(day_schedule) > 0:
-            book_name = input("Введи назву підручника:").capitalize()
-            if book_name in collected_books:
-                print("Ти вже поклав цю книгу")
-            elif book_name in day_schedule:
-                collected_books.append(book_name)
-                day_schedule.remove(book_name)
-                print("Добре Саня, вгадав. Все ж таки задатки розуму присутні")
+    for _ in range(len(day_schedule)):
+        book_name = input("Введи назву підручника:").capitalize()
+        if book_name in books:
+            if book_name in day_schedule:
+                if book_name in collected_books:
+                    print("Ти вже поклав цю книгу")
+                    print("Добре Саня, вгадав. Все ж таки задатки розуму присутні")
+                else:
+                    collected_books.append(book_name)
             else:
                 print("Олух ти, Санек! Давай ще раз)")
+        else:
+            print("Олух ти, Санек! Давай ще раз)")
     print(f"{print_books(collected_books)}. Санько, удачі в школі)")
     
     
@@ -22,7 +27,7 @@ def get_day_schedule():
     print(day)
     try:
         print(list(schedule[day]))
-        return list(schedule[day])
+        return schedule[day]
     except KeyError:
         print ("Саня, дивись на календар")
         return get_day_schedule()
